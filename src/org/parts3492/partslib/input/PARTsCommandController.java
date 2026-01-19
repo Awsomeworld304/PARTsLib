dd0fdfec52d567c21c022d52de364c96d551ec88
Awsmeworld304 <bucheckerr@gmail.com>
2026-01-19 13:40:19 -0500

    Fix formatting for PARTsCommandController.

--- src/org/parts3492/partslib/input/PARTsCommandController.java
+++ src/org/parts3492/partslib/input/PARTsCommandController.java
@@ -56,7 +56,7 @@ public class PARTsCommandController {
                 dualsenseController = new CommandPS5Controller(port);
                 break;
             case OTHER:
-                //joystick = new CommandJoystick(port);
+                // joystick = new CommandJoystick(port);
                 break;
             case XBOX:
                 xboxController = new CommandXboxController(port);
