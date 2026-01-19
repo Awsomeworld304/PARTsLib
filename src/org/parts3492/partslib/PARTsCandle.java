bddf59d0e09f415b15f08b8f2018d967b649104a
James <bucheckerr@gmail.com>
2026-01-18 23:34:18 -0500

    Add PARTsCandle class.

---
+++ src/org/parts3492/partslib/PARTsCandle.java
@@ -0,0 +1,406 @@
+package org.parts3492.partslib;
+
+import org.parts3492.partslib.command.PARTsCommandUtils;
+import org.parts3492.partslib.command.PARTsSubsystem;
+
+import com.ctre.phoenix6.CANBus;
+import com.ctre.phoenix6.configs.CANdleConfiguration;
+import com.ctre.phoenix6.controls.ColorFlowAnimation;
+import com.ctre.phoenix6.controls.ControlRequest;
+import com.ctre.phoenix6.controls.FireAnimation;
+import com.ctre.phoenix6.controls.LarsonAnimation;
+import com.ctre.phoenix6.controls.RainbowAnimation;
+import com.ctre.phoenix6.controls.RgbFadeAnimation;
+import com.ctre.phoenix6.controls.SingleFadeAnimation;
+import com.ctre.phoenix6.controls.SolidColor;
+import com.ctre.phoenix6.controls.StrobeAnimation;
+import com.ctre.phoenix6.controls.TwinkleAnimation;
+import com.ctre.phoenix6.controls.TwinkleOffAnimation;
+import com.ctre.phoenix6.hardware.CANdle;
+import com.ctre.phoenix6.signals.AnimationDirectionValue;
+import com.ctre.phoenix6.signals.LossOfSignalBehaviorValue;
+import com.ctre.phoenix6.signals.RGBWColor;
+import com.ctre.phoenix6.signals.StatusLedWhenActiveValue;
+import com.ctre.phoenix6.signals.StripTypeValue;
+import com.ctre.phoenix6.signals.VBatOutputModeValue;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+public abstract class PARTsCandle extends PARTsSubsystem {
+    // https://github.com/CrossTheRoadElec/Phoenix5-Examples/blob/master/Java%20General/CANdle%20MultiAnimation/src/main/java/frc/robot/subsystems/CANdleSystem.java
+    private static CANdle candle;
+    CANdleConfiguration config = new CANdleConfiguration();
+
+    public enum Color {
+
+        // Basic Colors
+        RED(255, 0, 0),
+        ORANGE(255, 165, 0),
+        YELLOW(255, 255, 0),
+        GREEN(0, 255, 0),
+        CYAN(0, 255, 255),
+        BLUE(0, 0, 255),
+        MAGENTA(255, 0, 255),
+        PURPLE(128, 0, 128),
+        WHITE(255, 255, 255),
+        BLACK(0, 0, 0),
+        GRAY(128, 128, 128),
+        LIGHT_GRAY(211, 211, 211),
+        DARK_GRAY(169, 169, 169),
+
+        // Red and Pink Tones
+        LIGHT_RED(255, 153, 153),
+        DARK_RED(139, 0, 0),
+        CRIMSON(220, 20, 60),
+        SALMON(250, 128, 114),
+        LIGHT_SALMON(255, 160, 122),
+        DARK_SALMON(233, 150, 122),
+        PINK(255, 192, 203),
+        LIGHT_PINK(255, 182, 193),
+        HOT_PINK(255, 105, 180),
+        DEEP_PINK(255, 20, 147),
+        MEDIUM_VIOLET_RED(199, 21, 133),
+
+        // Orange and Brown Tones
+        CORAL(255, 127, 80),
+        TOMATO(255, 99, 71),
+        ORANGE_RED(255, 69, 0),
+        GOLD(255, 215, 0),
+        BROWN(165, 42, 42),
+        SIENNA(160, 82, 45),
+        CHOCOLATE(210, 105, 30),
+        PERU(205, 133, 63),
+        SANDY_BROWN(244, 164, 96),
+        BEIGE(245, 245, 220),
+
+        // Yellow and Beige Tones
+        LIGHT_YELLOW(255, 255, 224),
+        LEMON_CHIFFON(255, 250, 205),
+        LIGHT_GOLDENROD_YELLOW(250, 250, 210),
+        KHAKI(240, 230, 140),
+        PALE_GOLDENROD(238, 232, 170),
+
+        // Green Tones
+        LIME(0, 255, 0),
+        LIME_GREEN(50, 205, 50),
+        LIGHT_GREEN(144, 238, 144),
+        PALE_GREEN(152, 251, 152),
+        DARK_GREEN(0, 100, 0),
+        FOREST_GREEN(34, 139, 34),
+        SEA_GREEN(60, 179, 113),
+        MEDIUM_SEA_GREEN(60, 179, 113),
+        LIGHT_SEA_GREEN(32, 178, 170),
+        SPRING_GREEN(0, 255, 127),
+        MEDIUM_SPRING_GREEN(0, 250, 154),
+
+        // Cyan and Blue Tones
+        AQUA(0, 255, 255),
+        LIGHT_CYAN(224, 255, 255),
+        TURQUOISE(64, 224, 208),
+        MEDIUM_TURQUOISE(72, 209, 204),
+        DARK_TURQUOISE(0, 206, 209),
+        CADET_BLUE(95, 158, 160),
+        STEEL_BLUE(70, 130, 180),
+        LIGHT_STEEL_BLUE(176, 196, 222),
+        POWDER_BLUE(176, 224, 230),
+        LIGHT_BLUE(173, 216, 230),
+        DEEP_SKY_BLUE(0, 191, 255),
+        SKY_BLUE(135, 206, 235),
+        MEDIUM_BLUE(0, 0, 205),
+        DARK_BLUE(0, 0, 139),
+        NAVY(0, 0, 128),
+        MIDNIGHT_BLUE(25, 25, 112),
+
+        // Purple and Violet Tones
+        VIOLET(238, 130, 238),
+        PLUM(221, 160, 221),
+        ORCHID(218, 112, 214),
+        MEDIUM_ORCHID(186, 85, 211),
+        DARK_ORCHID(153, 50, 204),
+        DARK_VIOLET(148, 0, 211),
+        INDIGO(75, 0, 130),
+        MEDIUM_PURPLE(147, 112, 219),
+        SLATE_BLUE(106, 90, 205),
+        DARK_SLATE_BLUE(72, 61, 139),
+        LAVENDER(230, 230, 250),
+        THISTLE(216, 191, 216);
+
+        public int r;
+        public int g;
+        public int b;
+
+        Color(int r, int g, int b) {
+            this.r = r;
+            this.g = g;
+            this.b = b;
+        }
+    }
+
+    private final int LED_LENGTH;
+
+    /** Creates a new light. */
+    public PARTsCandle(String className, int canID, int ledLength) {
+        super(className);
+        CANBus canbus = new CANBus("rio");
+        candle = new CANdle(canID, canbus);
+        LED_LENGTH = ledLength;
+
+        config.CANdleFeatures.StatusLedWhenActive = StatusLedWhenActiveValue.Disabled;
+        config.LED.LossOfSignalBehavior = LossOfSignalBehaviorValue.DisableLEDs;
+        config.LED.StripType = StripTypeValue.GRB;
+        config.LED.BrightnessScalar = 0.5;
+        config.CANdleFeatures.VBatOutputMode = VBatOutputModeValue.Modulated; // does this do anything?
+
+        applyConfig();
+
+        setNoColor();
+    }
+
+    /*---------------------------------- Custom Private Functions ---------------------------------*/
+    protected void setColor(Color color) {
+        candle.setControl(
+                new SolidColor(0, LED_LENGTH).withColor(new RGBWColor(color.r, color.g, color.b)));
+    }
+
+    protected void setNoColor() {
+        setColor(Color.BLACK);
+    }
+
+    protected Command setColorGreenCommand() {
+        return PARTsCommandUtils.setCommandName("CANdleColorGreen", runOnce(() -> setColor(Color.GREEN)));
+    }
+
+    protected Command setNoColorCommand() {
+        return PARTsCommandUtils.setCommandName("CANdleColorOff", runOnce(() -> setNoColor()));
+    }
+
+    private FireAnimation getBurnyBurnAnimation() {
+        return new FireAnimation(0, LED_LENGTH).withSparking(0.5).withCooling(0.5);
+    }
+
+    private RainbowAnimation getRainbowAnimation() {
+        return new RainbowAnimation(0, LED_LENGTH);
+    }
+
+    private StrobeAnimation getStrobeAnimation(Color color) {
+        return new StrobeAnimation(0, LED_LENGTH).withColor(new RGBWColor(color.r, color.g, color.b));
+    }
+
+    private StrobeAnimation getStrobeAnimation(Color color, double speed) {
+        return new StrobeAnimation(0, LED_LENGTH).withColor(new RGBWColor(color.r, color.g, color.b))
+                .withFrameRate(speed);
+    }
+
+    private SingleFadeAnimation getFadeAnimation(Color color) {
+        return new SingleFadeAnimation(0, LED_LENGTH)
+                .withColor(new RGBWColor(color.r, color.g, color.b));
+    }
+
+    private SingleFadeAnimation getFadeAnimation(Color color, double speed) {
+        return new SingleFadeAnimation(0, LED_LENGTH)
+                .withColor(new RGBWColor(color.r, color.g, color.b)).withFrameRate(speed);
+    }
+
+    protected void runBurnyBurnAnimation() {
+        setControl(getBurnyBurnAnimation());
+    }
+
+    protected void runRainbowAnimation() {
+        setControl(getRainbowAnimation());
+    }
+
+    protected void runStrobeAnimation(Color color) {
+        setControl(getStrobeAnimation(color));
+    }
+
+    protected void runStrobeAnimation(Color color, double speed) {
+        setControl(getStrobeAnimation(color, speed));
+    }
+
+    protected void runLarsonAnimation(Color color) {
+        setControl(getLarsonAnimation(color));
+    }
+
+    protected void runLarsonAnimation(Color color, double speed, int size) {
+        setControl(getLarsonAnimation(color, speed, size));
+    }
+
+    protected void runTwinkleAnimation(Color color) {
+        setControl(getTwinkleAnimation(color));
+    }
+
+    protected void runColorFlowAnimation(Color color) {
+        setControl(getColorFlowAnimation(color));
+    }
+
+    protected void runColorFlowAnimation(Color color, double speed, AnimationDirectionValue direction) {
+        setControl(getColorFlowAnimation(color, speed, direction));
+    }
+
+    protected void runFireAnimation() {
+        setControl(getFireAnimation());
+    }
+
+    protected void runFireAnimation(double brightness, double speed, double sparking, double cooling) {
+        setControl(getFireAnimation(brightness, speed, sparking, cooling));
+    }
+
+    protected void runTwinkleAnimation(Color color, double speed) {
+        setControl(getTwinkleAnimation(color, speed));
+    }
+
+    protected void runFadeAnimation(Color color, double speed) {
+        setControl(getFadeAnimation(color, speed));
+    }
+
+    private LarsonAnimation getLarsonAnimation(Color color) {
+        return new LarsonAnimation(0, LED_LENGTH).withColor(new RGBWColor(color.r, color.g, color.b));
+    }
+
+    private LarsonAnimation getLarsonAnimation(Color color, double speed,
+            int size) {
+        // Size max is 7
+        return new LarsonAnimation(0, LED_LENGTH).withColor(new RGBWColor(color.r, color.g, color.b));
+    }
+
+    private TwinkleAnimation getTwinkleAnimation(Color color) {
+        return new TwinkleAnimation(0, LED_LENGTH).withColor(new RGBWColor(color.r, color.g, color.b));
+    }
+
+    private TwinkleAnimation getTwinkleAnimation(Color color, double speed) {
+        return new TwinkleAnimation(0, LED_LENGTH).withColor(new RGBWColor(color.r, color.g, color.b))
+                .withFrameRate(speed);
+    }
+
+    private ColorFlowAnimation getColorFlowAnimation(Color color) {
+        return new ColorFlowAnimation(0, LED_LENGTH)
+                .withColor(new RGBWColor(color.r, color.g, color.b));
+    }
+
+    private ColorFlowAnimation getColorFlowAnimation(Color color, double speed, AnimationDirectionValue direction) {
+        return new ColorFlowAnimation(0, LED_LENGTH).withColor(new RGBWColor(color.r, color.g, color.b))
+                .withFrameRate(speed).withDirection(direction);
+    }
+
+    private FireAnimation getFireAnimation() {
+        return new FireAnimation(0, LED_LENGTH);
+    }
+
+    private FireAnimation getFireAnimation(double brightness, double speed, double sparking, double cooling) {
+        return new FireAnimation(0, LED_LENGTH).withBrightness(brightness).withFrameRate(speed)
+                .withSparking(sparking).withCooling(cooling);
+    }
+
+    private void setControl(ColorFlowAnimation a) {
+        candle.setControl(a);
+    }
+
+    private void setControl(FireAnimation a) {
+        candle.setControl(a);
+    }
+
+    private void setControl(LarsonAnimation a) {
+        candle.setControl(a);
+    }
+
+    private void setControl(RainbowAnimation a) {
+        candle.setControl(a);
+    }
+
+    private void setControl(RgbFadeAnimation a) {
+        candle.setControl(a);
+    }
+
+    private void setControl(SingleFadeAnimation a) {
+        candle.setControl(a);
+    }
+
+    private void setControl(StrobeAnimation a) {
+        candle.setControl(a);
+    }
+
+    private void setControl(TwinkleAnimation a) {
+        candle.setControl(a);
+    }
+
+    private void setControl(TwinkleOffAnimation a) {
+        candle.setControl(a);
+    }
+
+    private void setControl(ControlRequest a) {
+        candle.setControl(a);
+    }
+
+    private void applyConfig() {
+        candle.getConfigurator().apply(config);
+    }
+
+    /*---------------------------------- Custom Public Functions ----------------------------------*/
+
+    /* Wrappers so we can access the CANdle from the subsystem */
+    public double getVbat() {
+        return candle.getVBatModulation(true).getValueAsDouble();
+    }
+
+    public double get5V() {
+        return candle.getFiveVRailVoltage(true).getValueAsDouble();
+    }
+
+    public double getCurrent() {
+        return candle.getOutputCurrent(true).getValueAsDouble();
+    }
+
+    public double getTemperature() {
+        return candle.getDeviceTemp(true).getValueAsDouble();
+    }
+
+    public void configBrightness(double percent) {
+        config.LED.BrightnessScalar = percent;
+        applyConfig();
+    }
+
+    public void configLos(LossOfSignalBehaviorValue disableWhenLos) {
+        config.LED.LossOfSignalBehavior = disableWhenLos;
+        applyConfig();
+    }
+
+    public void configLedType(StripTypeValue type) {
+        config.LED.StripType = type;
+        applyConfig();
+    }
+
+    public void configStatusLedBehavior(StatusLedWhenActiveValue offWhenActive) {
+        config.CANdleFeatures.StatusLedWhenActive = offWhenActive;
+        applyConfig();
+    }
+
+    /*-------------------------------- Generic Subsystem Functions --------------------------------*/
+
+    @Override
+    public void periodic() {
+        // This method will be called once per scheduler run
+    }
+
+    @Override
+    public void outputTelemetry() {
+        super.partsNT.putString("Animation", candle.getAppliedControl().getName());
+    }
+
+    @Override
+    public void stop() {
+        // TODO Auto-generated method stub
+        // throw new UnsupportedOperationException("Unimplemented method 'stop'");
+    }
+
+    @Override
+    public void reset() {
+        // TODO Auto-generated method stub
+        // throw new UnsupportedOperationException("Unimplemented method 'reset'");
+    }
+
+    @Override
+    public void log() {
+        // TODO Auto-generated method stub
+        super.partsLogger.logString("Animation", candle.getAppliedControl().getName());
+    }
+}
